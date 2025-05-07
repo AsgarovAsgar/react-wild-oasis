926f8eac8824d9bec56f0de0eafc3052fd25ae03
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-05-07 11:03:20 +0400

    add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button"
+import Input from "./ui/Input"
+
+const H1 = styled.h1`
+  font-size:30px;
+  font-weight: 600;
+`
+
+
+
+const StyledApp = styled.div`
+  display: flex;
+  gap: 20px;
+  background-color: var(--color-brand-500);
+`
+
 export default function App() {
-  return <div>hello</div>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Wild oss</H1>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  )
 }

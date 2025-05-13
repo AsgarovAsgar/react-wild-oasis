4ed660adf23a04f0f493c0e02614e6ca9d1c6137
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-05-13 11:31:52 +0400

    fetch app settings

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Account from './pages/Account'
 import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import { Toaster } from "react-hot-toast"
+import Settings from "./pages/Settings"
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -37,7 +38,7 @@ function App() {
             <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
-            <Route path="settings" element={<Dashboard />} />
+            <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
           

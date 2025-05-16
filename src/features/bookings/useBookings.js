1e9052767b51a9a569e063689a209f0c81b95792
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-05-16 14:22:19 +0400

    add api side sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -10,11 +10,15 @@ export default function useBookings() {
     ? null 
     : { field: "totalPrice", value: 5000, method: "gte" };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
 
 
   const { isLoading, data: bookings } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter })
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy })
   })
 
   return { isLoading, bookings }

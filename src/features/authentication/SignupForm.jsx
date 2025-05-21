390b266a243e8a214fa3a5b68eb7cd6fd6a27382
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-05-21 15:40:29 +0400

    add sign up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,29 +1,34 @@
+import { useForm } from "react-hook-form";
+
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-import { useForm } from "react-hook-form";
 
+import { useSignup } from "./useSignup";
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState; 
-
-  const onSubmit = (data) => {
-    console.log(data);
+  const { signup, isLoading } = useSignup();
+  const onSubmit = ({fullName, email, password}) => {
+    signup({ fullName, email, password }, {
+      onSettled: () => reset()
+    });
   };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input type="text" id="fullName" {...register("fullName", { required: "This field is required" })} />
+        <Input type="text" id="fullName" disabled={isLoading} {...register("fullName", { required: "This field is required" })} />
       </FormRow>
 
       <FormRow label="Email address" error={errors?.email?.message}>
-        <Input 
+          <Input 
           type="email" 
           id="email" 
+          disabled={isLoading}
           {...register("email", { 
             required: "This field is required", 
             pattern: { value: /\S+@\S+\.\S+/, message: "Please enter a valid email address" } 
@@ -35,6 +40,7 @@ function SignupForm() {
         <Input 
           type="password" 
           id="password" 
+          disabled={isLoading}
           {...register("password", { 
             required: "This field is required", 
             minLength: { value: 8, message: "Password must be at least 8 characters long" } 
@@ -46,6 +52,7 @@ function SignupForm() {
         <Input 
           type="password" 
           id="passwordConfirm" 
+          disabled={isLoading}
           {...register("passwordConfirm", { 
             required: "This field is required", 
             validate: (value) => value === getValues("password") || "Passwords do not match" 

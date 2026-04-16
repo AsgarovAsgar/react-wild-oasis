bc276caa7229bd533df86f57e8d20651c0bfe1f1
Asgar Asgarov <asgarovasgar28@gmail.com>
2026-04-16 19:10:26 +0400

    update login form to allow user to view demo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,11 +13,33 @@ const LoginLayout = styled.main`
   background-color: var(--color-grey-50);
 `;
 
+const InfoBox = styled.div`
+  background-color: var(--color-blue-100);
+  border: 1px solid var(--color-blue-700);
+  border-radius: var(--border-radius-md);
+  padding: 1.6rem 2rem;
+  text-align: center;
+  color: var(--color-blue-700);
+  font-size: 1.4rem;
+  line-height: 1.6;
+
+  & strong {
+    font-weight: 600;
+    color: var(--color-blue-800);
+  }
+`;
+
 function Login() {
   return (
     <LoginLayout>
       <Logo />
       <Heading as="h4">Log in to your account</Heading>
+      <InfoBox>
+        <strong>Demo Access Available</strong>
+        <br />
+        The email and password fields are prefilled for you to easily explore
+        the internal admin dashboard.
+      </InfoBox>
       <LoginForm />
     </LoginLayout>
   );
